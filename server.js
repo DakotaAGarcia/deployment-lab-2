815d6f0cbf4ff711e15cc76fe1a63f25ffddea13
Dakota Garcia <dakotagarcia@Dakotas-MacBook-Air.local>
2023-01-26 16:56:22 -0700

    about-contact-projects-added

--- server.js
+++ server.js
@@ -3,8 +3,38 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(`${__dirname}/public`))
-
-
-app.listen(4000, () =>{
-    console.log('ready to rock on 4000')
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f6bc1887986e4cafb737560f61776aa1',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
 })
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
+// try {
+//     nonExistentFunction();
+//   } catch (error) {
+//     rollbar.error('error')
+//     console.error(error);
+//   }
+//   try {
+//     nonExistentFunction();
+//   } catch (error) {
+//     rollbar.warning('warning')
+//     console.error(error);
+//   }
+//   try {
+//     nonExistentFunction();
+//   } catch (error) {
+//     rollbar.critical('critical error')
+//     console.error(error);
+//   }
+
+// app.listen(4000, () =>{
+//     console.log('ready to rock on 4000')
+// })
